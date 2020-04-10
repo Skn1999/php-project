f5765ff842f23738263339fdd31ca62f9a66c13d
Nayan Kumar <46842728+KrNayan@users.noreply.github.com>
2020-04-10 10:54:23 +0530

    changes in Return Values

--- jLoginPhp/jLogin.php
+++ jLoginPhp/jLogin.php
@@ -11,19 +11,20 @@
 	$selQryRst=mysqli_query($con,$selQry) or die(mysqli_error($con));
 	$x=mysqli_num_rows($selQryRst);
 
+	// echo $x;
+
 	if($x>0){
 
 		$_SESSION['email'] = $email;
 		if(strcmp($orgTyp,'institute')==0)
-			header('Location: ../includes/institute.php');
+			echo "institute";
 		else  if(strcmp($orgTyp,'company')==0)
-			header('Location: ../includes/company.php');
-
+			echo "company";
+		return;
 	}
 
-	echo "<h3>invalid Username/Password</h3>";
-	echo "After 7 seconds you will be redirected back to Login Page";
-	header('refresh:7; url = ../includes/login.php');
+	echo "error";
+	// echo "After 7 seconds you will be redirected back to Login Page";
+	// header('refresh:7; url = ../includes/login.php');
 	
-
 ?>
